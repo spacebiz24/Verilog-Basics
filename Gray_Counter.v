c4d8868552a4da58f2b3484fd95acf44e10d7881
Siddhaanth S Iyer <78657717+spacebiz24@users.noreply.github.com>
2022-11-08 10:45:40 +0530

    Update Gray_Counter.v

--- Gray_Counter.v
+++ Gray_Counter.v
@@ -21,18 +21,18 @@
 
 
 module Gray_Counter(clk, out, rst);
-input clk, rst;
-reg [3:0] outb;
-output reg [3:0] out;
-integer i;
-always @ (posedge clk)
-begin
-    if(!rst)
-        outb <= 0;
-    else
-        outb <= outb+1;
-    for (i = 0; i < 3; i = i + 1)
-        out[i] = outb[i] ^ outb[i+1];
-    out[3] = outb[3];
-end
+    input clk, rst;
+    reg [3:0] outb;
+    output reg [3:0] out;
+    integer i;
+    always @ (posedge clk)
+    begin
+        if(!rst)
+            outb <= 0;
+        else
+            outb <= outb+1;
+        for (i = 0; i < 3; i = i + 1)
+            out[i] = outb[i] ^ outb[i+1];
+        out[3] = outb[3];
+    end
 endmodule
